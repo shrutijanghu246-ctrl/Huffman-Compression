b537f893ced189146790fcfc2673b989cd8bf4e2
shrutijanghu246-ctrl <shrutijanghu246@gmail.com>
2026-05-18 23:39:01 +0530

    add buld tree function

--- huffman.cpp
+++ huffman.cpp
@@ -12,3 +12,23 @@ unordered_map<char, int> buildFreqTable(const string& text){
 
     return freq;
 }
+
+HuffmanNode* buildTree(unordered_map<char, int>& freq){
+    priority_queue<HuffmanNode*, vector<HuffmanNode*>, NodeCmp> pq;
+
+    // one leaf node for each character and push to queue
+    for(auto& pair: freq){
+        pq.push(new HuffmanNode(pair.first, pair.second));
+    } 
+
+    //merging until one node remains
+    while(pq.size() > 1){
+        HuffmanNode* left = pq.top(); pq.pop();
+        HuffmanNode* right = pq.top(); pq.pop();
+
+        int mergedFreq = left->freq + right->freq;
+        pq.push(new HuffmanNode(mergedFreq, left, right));
+    }
+
+    return pq.top();
+}
